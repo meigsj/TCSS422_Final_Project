9e7297bcc42b222c97bcd3fd9194093449cb9c63
Shaun Coleman <shaunpcole@gmail.com>
2017-12-01 11:45:27 -0800

    Fixes to SimpleStack and Stack usage in syncro dispatchers

--- Simple_Stack.c
+++ Simple_Stack.c
@@ -12,25 +12,31 @@ Joshua Meigs
 SIMPLE_STACK_p createSimpleStack() {
 	SIMPLE_STACK_p stack = (SIMPLE_STACK_p)malloc(sizeof(SIMPLE_STACK_s));
 	stack->head = NULL;
+	stack->size = 0;
 	return stack;
 }
 
 void ss_push(SIMPLE_STACK_p stack, int value) {
 	STACK_NODE_p node = (STACK_NODE_p)malloc(sizeof(STACK_NODE_s));
 	node->next = stack->head;
+	node->value = value;
 	stack->head = node;
+	stack->size++;
 }
 
 int ss_pop(SIMPLE_STACK_p stack) {
+	assert(stack->size != 0);
 	int retVal = stack->head->value;
 	STACK_NODE_p next = stack->head->next;
 	free(stack->head);
 	stack->head = next;
+	stack->size--;
 
 	return retVal;
 }
 
 int ss_peek(SIMPLE_STACK_p stack) {
+	assert(stack->size != 0);
 	return stack->head->value;
 }
 
@@ -44,6 +50,10 @@ void destructStack(SIMPLE_STACK_p stack) {
 	while (!stack_is_empty(stack)) ss_pop(stack);
 
 	free(stack);
+}
+
+int ss_getSize(SIMPLE_STACK_p stack) {
+	return stack->size;
 }
 
 
